5fd52e94bfaac25568b19b2708cd1f91092c3c70
LittleDarkPenguin <darkywizard@gmail.com>
2021-09-29 17:53:55 +0700

    little refactoring added getting specific type object via predicate (single and list<>)
    TODO add support for different prefab with the same script on them

--- ExamplePool.cs
+++ ExamplePool.cs
@@ -1,7 +1,61 @@
+public enum AttackType
+{
+    melee,
+    distance
+}
+
 public class Enemy : MonoBehaviour
 {
+	[SerializeField] protected int health;
+    public int Health => health;
+    [SerializeField] protected AttackType attackType;
+    public AttackType AttackType => attackType;
+}
+
+public class Mage : Enemy
+{	
+}
+
+public class Warrior : Enemy
+{	
+}
+
+public class Spider : Enemy
+{	
 }
 
 public class ExamplePool : ObjectPool<Enemy>
 {
+	private void Start()
+    {
+        Vector2 pos = Vector2.zero;
+        
+        if (TryGetRandomObject(out Enemy randomEnemy))
+        {
+            randomEnemy.transform.position = pos;
+            pos.x++;
+        }
+        
+        if (TryGetFirstSpecificObject(x => x.Health > 10, out Enemy specificEnemy))
+        {
+            specificEnemy.transform.position = pos;
+            pos.x++;
+        }
+
+        if (TryGetFirstSpecificObject(x => x.GetType() == typeof(Warrior), out Enemy warriorEnemy))
+        {
+            warriorEnemy.transform.position = pos;
+            pos.x++;
+        }
+        
+        if (TryGetSpecificObjects(x => x.AttackType == AttackType.melee,
+            10, out List<Enemy> specificEnemies, false))
+        {
+            foreach (var enemy in specificEnemies)
+            {
+                enemy.transform.position = pos;
+                pos.x += 1;
+            }
+        }
+	}
 }
--- ObjectPool.cs
+++ ObjectPool.cs
@@ -1,85 +1,160 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public abstract class ObjectPool<P> : MonoBehaviour where P : Component
+public class ObjectPool<P> : MonoBehaviour where P : MonoBehaviour
 {
-    [SerializeField] protected P[] objPrefabs;
-    [SerializeField] protected int initialAmount = 10;
-    
-    private Dictionary<System.Type, Queue<P>> pool = new Dictionary<System.Type, Queue<P>>();
-    private List<System.Type> keys;
-    
+    [SerializeField] private P[] prefabs;
+    [SerializeField] private int initialAmount = 10;
+
+    private Dictionary<P, Queue<P>> pool;
+    private List<P> keys;
+
     private void Awake()
     {
-        foreach (var enemy in objPrefabs)
-        {
-            InitialFill(enemy, initialAmount);
-        }
-        keys = new List<System.Type>(pool.Keys);
+        InitialFill();
     }
 
-    private void InitialFill(P prefab, int amount = 10)
+    private void InitialFill()
     {
-        var type = prefab.GetType();
-        if (!pool.ContainsKey(type))
+        pool = new Dictionary<P, Queue<P>>();
+        foreach (var pref in prefabs)
         {
-            pool.Add(type, new Queue<P>());
-            for (int i = 0; i < amount; i++)
+            if (!pool.ContainsKey(pref))
             {
-                P obj = Instantiate(prefab, Vector3.zero, Quaternion.identity, transform);
-                pool[type].Enqueue(obj);
-                obj.gameObject.SetActive(false);
+                pool.Add(pref, new Queue<P>());
+                
+                for (int i = 0; i < initialAmount; i++)
+                {
+                    var obj = Instantiate(pref, Vector3.zero, Quaternion.identity, transform);
+                    obj.gameObject.SetActive(false);
+                    pool[pref].Enqueue(obj);
+                }
+            }
+            else
+            {
+                Debug.Log($"Trying to add existing prefab \"{pref}\" to pool, skipping");
             }
         }
-        else
-        {
-            Debug.LogError($"Trying to add existing type |{prefab}| to pool, skipping");
-        }
-
+        keys = new List<P>(pool.Keys);
     }
 
-    public void ReturnObjectToPool(P obj)
+    private bool TryGetQueueForType(P type, out Queue<P> queue)
     {
-        var type = obj.GetType();
-        if (pool.TryGetValue(type, out Queue<P> queue))
+		// Really dont like this
+        queue = null;
+        foreach (var key in pool.Keys)
         {
-            obj.gameObject.SetActive(false);
-            queue.Enqueue(obj);
+            if (key.GetType() == type.GetType())
+            {
+                queue = pool[key];
+                return true;
+            }
         }
-        else
-        {
-            Debug.Log($"Returning object with non existing type in pool |{type}|, adding new queue");
-            var newQueue = new Queue<P>();
-            pool.Add(type, newQueue);
-            pool[type].Enqueue(obj);
-        }
+        return false;
     }
-
-    private P TryGetObjectByType(System.Type type)
+    
+    private P TryGetObjectByType(P type)
     {
-        if (pool.TryGetValue(type, out Queue<P> queue))
+        if (TryGetQueueForType(type, out var queue))
         {
             if (queue.Count > 1)
             {
-                P obj = queue.Dequeue();
+                var obj = queue.Dequeue();
                 obj.gameObject.SetActive(true);
                 return obj;
             }
             else
             {
                 Debug.Log($"Queue for |{type}| almost empty, creating new obj for it");
-                P obj = Instantiate(queue.Peek(), Vector3.zero, Quaternion.identity, transform);
+                var obj = Instantiate(queue.Peek(), Vector3.zero, Quaternion.identity, transform);
+                obj.gameObject.SetActive(true);
                 return obj;
             }
         }
-        Debug.LogError($"Asking for non existing type in pool |{type}|, returning null");
+        Debug.LogError($"Asking for non existing object in pool \"{type}\" (what is nearly impossible btw), returning null");
         return null;
     }
     
-    public P GetRandomObject()
+    public bool TryGetRandomObject(out P obj)
     {
+        obj = null;
+        if (pool.Count == 0)
+        {
+            Debug.LogError("Pool is empty, returning null");
+            return false;
+        }
         int randomIndex = Random.Range(0, keys.Count);
-        return TryGetObjectByType(keys[randomIndex]);
+        obj = TryGetObjectByType(keys[randomIndex]);
+        return true;
+    }
+    
+    public void ReturnObjectToPool(P obj)
+    {
+        if (TryGetQueueForType(obj, out var queue))
+        {
+            queue.Enqueue(obj);
+            obj.gameObject.SetActive(false);
+            return;
+        }
+
+        Debug.Log($"Returning object that doesn't exist in pool \"{obj}\", adding new queue");
+        var newQueue = new Queue<P>();
+        pool.Add(obj, newQueue);
+        pool[obj].Enqueue(obj);
+        obj.gameObject.SetActive(false);
+    }
+
+    public bool TryGetFirstSpecificObject(Predicate<P> propertySelector, out P obj)
+    {
+        obj = null;
+        var type = keys.Find(propertySelector);
+        if (type == null)
+        {
+            Debug.Log("Nothing was found by your request, returning null");
+            return false;
+        }
+        obj = TryGetObjectByType(type);
+        obj.gameObject.SetActive(true);
+        return true;
+    }
+
+    public bool TryGetSpecificObjects(Predicate<P> propertySelector, int amount, out List<P> objects, bool randomize = true)
+    {
+        objects = null;
+        amount = Math.Abs(amount);
+        // Really dont like this
+        var types = keys.FindAll(propertySelector);
+        if (types.Count == 0)
+        {
+            Debug.Log("Nothing was found by your request");
+            return false;
+        }
+
+        objects = new List<P>(amount);
+        if (randomize)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                objects.Add(TryGetObjectByType(types[Random.Range(0, types.Count)]));
+            }
+        }
+        else
+        {
+            int amountOfType = amount / types.Count;
+            foreach (var t in types)
+            {
+                for (int i = 0; i < amountOfType; i++)
+                {
+                    objects.Add(TryGetObjectByType(t));
+                }
+            }
+            if (amount % types.Count != 0)
+            {
+                objects.Add(TryGetObjectByType(types[Random.Range(0, types.Count)]));
+            }
+        }
+        return true;
     }
 }
